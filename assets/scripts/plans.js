a5a2d880df7f6668ab4963a39e7090d7995d67b7
AugustoTI <augustocesar4001@gmail.com>
2025-06-16 02:40:42 -0300

    refactor: plans

---
+++ assets/scripts/plans.js
@@ -0,0 +1,23 @@
+const currencyFormatter = new Intl.NumberFormat("pt-BR", {
+  currency: "BRL",
+  style: "currency",
+});
+
+document.querySelectorAll(".plan").forEach((plan) => {
+  const base = parseFloat(plan.dataset.base);
+  const totalEl = plan.querySelector(".plan-info-price");
+  const checkboxes = plan.querySelectorAll('input[type="checkbox"]');
+
+  const updatePrice = () => {
+    let total = base;
+    checkboxes.forEach((cb) => {
+      if (cb.checked) total += parseFloat(cb.dataset.extra);
+    });
+
+    const totalAsNumber = Number(total.toFixed(2));
+
+    totalEl.textContent = currencyFormatter.format(totalAsNumber);
+  };
+
+  checkboxes.forEach((cb) => cb.addEventListener("change", updatePrice));
+});
